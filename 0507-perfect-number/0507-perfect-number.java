ac1113c8ee7df6bdc6fed958bb7609971f123112
TanikaThiagu <reachtanikathiagu@gmail.com>
2025-09-09 14:44:32 +0530

    Time: 2113 ms (9.08%), Space: 40.5 MB (57.08%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        
+        int sum =0 ;
+        for(int div = 1 ; div<=num/2 ; div++)
+        {
+            if(num%div==0)
+             sum=sum+div;
+
+        }
+        return (sum==num);
+    }
+}
