78cf64542d41235f642095cdb7dc4da1b2622bd3
TanikaThiagu <reachtanikathiagu@gmail.com>
2025-10-09 15:36:02 +0530

    Time: 533 ms (53.93%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select user_id , distinct(COUNT(follower_id)) AS followers_count from followers 
-GROUP BY follower_id
+select user_id , COUNT(follower_id) AS followers_count from followers 
+GROUP BY user_id
  order by user_id ASC;
