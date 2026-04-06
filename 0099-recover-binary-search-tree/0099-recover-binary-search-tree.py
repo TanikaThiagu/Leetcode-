0f53f159ecfe710c4795b162670fce789992268f
TanikaThiagu <reachtanikathiagu@gmail.com>
2026-04-06 22:25:22 +0530

    Time: 13 ms (8.13%), Space: 12.8 MB (35.04%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def recoverTree(self, root):
+        first = second = prev = None
+        curr = root
+        
+        while curr:
+            if not curr.left:
+                # Visit node
+                if prev and prev.val > curr.val:
+                    if not first:
+                        first = prev
+                    second = curr
+                prev = curr
+                curr = curr.right
+            else:
+                # Find inorder predecessor
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
+                
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    
+                    # Visit node
+                    if prev and prev.val > curr.val:
+                        if not first:
+                            first = prev
+                        second = curr
+                    prev = curr
+                    
+                    curr = curr.right
+        
+        # Swap values
+        first.val, second.val = second.val, first.val
